e1db47f046bfb567daf35cf2856851175e1bfdb4
diazoangga <91169005+diazoangga@users.noreply.github.com>
2023-07-14 23:08:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from streamlit_extras.add_vertical_space import add_vertical_space
+# from streamlit_extras.add_vertical_space import add_vertical_space
 from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
